e64e3398dc7a14d46fbcc1cde9397508bc895e00
sinat <sinatnang1998@gmail.com>
2022-09-15 14:30:58 +0700

    commit register

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
 import React, { Component } from 'react';
-import { Alert, Button,Text, TextInput, View, StyleSheet,TouchableOpacity } from 'react-native';
-import Registers from './app/components/Registers';
-//import Product from './app/components/Product';
+import { Alert, Button,Text, TextInput, View, StyleSheet,TouchableOpacity,ScrollView, FlatList } from 'react-native';
+//import Registers from './app/components/Registers';
+import Product from './app/components/Product';
 import {products} from './products.json';
 export default class App extends Component {
   render() {
+      const Products = () => {
+          return (
+              // <ScrollView style={styles.scrollView}>
+              //     {
+              //     products.map(product => {
+              //         console.log('Production=======>',products);
+              //         return(
+              //            <Product
+              //                 key={product.id}
+              //                 id={product.id}
+              //                 title={product.title}
+              //                 description={product.description}
+              //                 image={product.thumbnail}
+              //            />
+              //         );
+              //     })
+              //     }
+              // </ScrollView>
+              <FlatList 
+                data={products} 
+                renderItem={prop =>{
+                // console.log(prop.item.title)
+                <Product 
+                  key={prop.item.id} 
+                  id={prop.item.id}
+                  title={prop.item.title} 
+                  description={prop.item.description}  
+                  image={prop.item.thumbnail} />
+              }}
+              />
+          );
+       };
     return (
       <View style={styles.container}>
-        <Registers/>
+        <Products/>
       </View>
     );
   }
@@ -17,10 +49,10 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex:1,
-     justifyContent:'center',
-     backgroundColor:'#5D6D7E',
-     paddingLeft:60,
-     paddingRight:60,
+//     justifyContent:'center',
+//     backgroundColor:'#5D6D7E',
+//     paddingLeft:60,
+//     paddingRight:60,
   },
 
 });
--- app/components/Product.js
+++ app/components/Product.js
@@ -1,28 +1,10 @@
 import React, { Component } from 'react';
-import { Alert, Button,Text, TextInput, View, StyleSheet } from 'react-native';
+import { Alert, Button,Text, TextInput, View, StyleSheet,TouchableWithoutFeedback,Image } from 'react-native';
  export default class Product extends Component {
   render() {
-  const Products=()=>{
-              return{
-               <ScrollView>
-                  {products.map(product => {
-                      console.log{'production ======>',product}
-                      return{
-                          <Product
-                              key={product.id}
-                              id={product.id}
-                              title={product.title}
-                              description={product.description}
-                              image={product.thumbnail}
-                           />
-                      }
-                  })}
-               </ScrollView>
-              }
-            }
     return (
       <View style={styles.myStyle}>
-        <TouchableWithoutFeedback onPress={()=>alert(this.props.id)}
+        <TouchableWithoutFeedback onPress={()=>alert(this.props.id)}>
             <Text style={styles.title}>{this.props.title}</Text>
         </TouchableWithoutFeedback>
         <Text>{this.props.description}</Text>
@@ -40,9 +22,11 @@ const styles = StyleSheet.create({
   },
   title:{
     fontSize:25,
-  }
+  },
   myStyle:{
     margin:8,
+    borderRadius:12,
+    overFlow:'hidden',
   }
 
 });
